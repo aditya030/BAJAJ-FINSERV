b384418ffdbc625a734f1bce6d8464a6e4bf0f4a
Aditya Sinha <61179961+aditya030@users.noreply.github.com>
2024-08-25 12:26:25 +0530

    adding

---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const router = new express.Router();
+
+// GET route to return the operation code
+router.get('/bfhl', (req, res) => {
+    try {
+        res.status(200).json({ operation_code: 1 });
+    } catch (e) {
+        res.status(500).json({
+            message: "Internal Server Error!"
+        });
+    }
+});
+
+// POST route to process the input data
+router.post('/bfhl', (req, res) => {
+    const { data } = req.body;
+    const user_id = "Aditya_Sinha_03072003";
+    const email = "aditya.sinha2021@vitstudent.ac.in";
+    const roll_number = "21BKT0022";
+
+    if (!data || !Array.isArray(data)) {
+        return res.status(400).json({ is_success: false });
+    }
+
+    const numbers = data.filter(item => !isNaN(item));
+    const alphabets = data.filter(item => /^[a-zA-Z]$/.test(item));
+    
+   
+    const lowercaseAlphabets = alphabets.filter(item => /^[a-z]$/.test(item));
+    const highest_alphabet = lowercaseAlphabets.length > 0 ? [lowercaseAlphabets.sort().pop()] : [];
+
+    res.json({
+        is_success: true,
+        user_id,
+        email,
+        roll_number,
+        numbers,
+        alphabets,
+        highest_alphabet
+    });
+});
+
+module.exports = router;
